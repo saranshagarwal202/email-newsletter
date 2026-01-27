9f689916dc1d296f7c7b517141be77e142266b9b
Saransh Agrawal <saranshagarwal2020@gmail.com>
2026-01-27 11:08:58 -0800

    Added serde to deserialize form data in subscribe API

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,29 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use actix_web::{App, HttpResponse, HttpServer, web};
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-            App::new()
-                .route("/health_check", web::get().to(health_check))
-        })
-        .listen(listner)?
-        .run();
-        // .await();
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listner)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,14 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listner.local_addr().unwrap().port();
+
+    let server = email_newsletter::run(listner).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -13,20 +22,52 @@ async fn health_check_works() {
         .await
         .expect("Failed to execute request");
 
-    
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-
 }
 
-fn spawn_app() -> String {
+#[tokio::test]
+async fn subscribe_returns_valid_200() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
 
-    let listner = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
 
-    let port = listner.local_addr().unwrap().port();
+    assert_eq!(200, response.status().as_u16());
+}
 
-    let server = email_newsletter::run(listner).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+
+#[tokio::test]
+async fn subscribe_returns_400_data_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "email missing"),
+        ("email=ursula_guin%40gmail.com", "name missing"),
+        ("", "email and name missing")
+    ];
+
+    for (body, message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400, response.status().as_u16(), "The API did not fail with body {}", message
+        );
+    }
+
+    
 }
